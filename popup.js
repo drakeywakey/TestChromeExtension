4f109d15f12e104bd85a9c6a7c7082c1e121bbc6
Drake Bennion <drakebennion@gmail.com>
2016-12-16 14:23:03 -0700

    Oh. Google deprecated that image search api IN 2011. Good looking out, me

--- popup.js
+++ popup.js
@@ -4,13 +4,22 @@
 	3. Display the image in the extension popup
 */
 document.addEventListener('DOMContentLoaded', function () {
+	var image = document.querySelector('img');
+	var status = document.getElementById('status');
+
 	getCurrentTabUrl(function(url) {
-		document.getElementById('status').textContent = 'Performing image search for ' + url;
+		status.textContent = 'Performing image search for ' + url;
 
 		getImageUrl(url, function (imgUrl, width, height) {
+			status.textContent = 'Searched for: ' + url + '\n' + 'Google Image Search result: ' + imgUrl;
+
+			image.width = width;
+			image.height = height;
+			image.src = imgUrl;
+			image.hidden = false;
 
 		}, function (error) {
-
+			status.textContent = 'Could not display the image: ' + error;
 		});
 	});
 });
@@ -36,9 +45,35 @@ function getCurrentTabUrl(callback) {
 
 // Uses google's image search api
 function getImageUrl(url, callback, errorFunc) {
+	var firstResult;
 	var request = new XMLHttpRequest();
+	var response;
 	var searchUrl ='https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=' + encodeURIComponent(url);
 
 	request.open('GET', searchUrl);
 	request.responseType = 'json';
+
+	request.onload = function () {
+		response = request.response;
+		if (!(response && response.responseData && response.responseData.results && response.responseData.results.length)) {
+			errorFunc('The image search returned no results.');
+			return;
+		}
+
+		firstResult = response.responseData.results[0];
+
+		imageUrl = firstResult.tbUrl;
+		width = parseInt(firstResult.tbWidth);
+		height = parseInt(firstResult.tbHeight);
+
+		//find out what console.assert does -- tutorial uses it here to assert the imageUrl, height and width all make sense.
+
+		callback(imageUrl, width, height);
+	};
+
+	request.onerror = function () {
+		errorFunc('Something went wrong :/');
+	};
+
+	request.send();
 }
